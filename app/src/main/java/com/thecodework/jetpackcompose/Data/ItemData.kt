3ede781ea917c3dd6f35bb8946da5132d4e016d9
amrutaradke <amruta.p.radke@gmail.com>
2022-04-14 19:22:37 +0530

    Added data list and images of list

---
+++ app/src/main/java/com/thecodework/jetpackcompose/Data/ItemData.kt
@@ -0,0 +1,29 @@
+package com.thecodework.jetpackcomposefirstapp.Data
+
+import com.thecodework.jetpackcompose.R
+
+object ItemData {
+  val list = listOf(
+    ItemName(name = "Sugar",
+      price = "$55.00",
+      image = R.drawable.sugar
+    ),
+    ItemName(name = "Coffe",
+      price = "$110.00",
+      image = R.drawable.coffe
+    ),
+    ItemName(name = "Moong Dal",
+      price = "$100.00",
+      image = R.drawable.moongdal
+    ),
+    ItemName(name = "Little Heart",
+      price = "$20.00",
+      image = R.drawable.littlehearts
+    ),
+    ItemName(name = "Red Lable Tea",
+      price = "$200.00",
+      image = R.drawable.red_label_tea
+    )
+  )
+
+}
---
+++ app/src/main/java/com/thecodework/jetpackcompose/Data/ItemName.kt
@@ -0,0 +1,7 @@
+package com.thecodework.jetpackcomposefirstapp.Data
+
+data class ItemName(
+  val name: String,
+  val price: String,
+  val image: Int
+  )
